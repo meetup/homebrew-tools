5bc474a753e693cd1536d8a4132dec8e826bec15
softprops <d.tangren@gmail.com>
2017-06-19 00:58:10 -0400

    bump version

--- rebazel.rb
+++ rebazel.rb
@@ -3,7 +3,7 @@ class Rebazel < Formula
   homepage "https://github.com/meetup/rebazel"
   url "https://github.com/meetup/rebazel/releases/download/v0.1.0/rebazel-Darwin-x86_64.tar.gz"
   sha256 "b19ea6ca9615666a4fb688a804dc8ddddd24b2beec4a7519948e0cd618664702"
-  version "v0.0.0"
+  version "v0.1.0"
 
   bottle :unneeded
 

0f3ccf8aca78284159ee2dca82ceb48ad322e13c
softprops <d.tangren@gmail.com>
2019-02-01 17:14:35 -0500

    logs

--- rebazel.rb
+++ rebazel.rb
@@ -2,7 +2,7 @@ class Rebazel < Formula
   desc "a file watch tool for rapid bazel development"
   homepage "https://github.com/meetup/rebazel"
   url "https://github.com/meetup/rebazel/releases/download/v0.1.1/rebazel-Darwin-x86_64.tar.gz"
-  sha256 "953a975c76d336fccffa575f5a15f949f917f2496409a2375692c2ac4ba226b2"
+  sha256 "6668f6a87a2e50e5dff3c93d2e8bf11c699aaf26252742ec6c66ea6306d3b59e"
   version "v0.1.1"
 
   bottle :unneeded

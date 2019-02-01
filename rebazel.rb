143514848c9cdd67aa7bcdac74bd50d919c4d35f
softprops <d.tangren@gmail.com>
2019-02-01 17:27:17 -0500

    update version

--- rebazel.rb
+++ rebazel.rb
@@ -1,9 +1,9 @@
 class Rebazel < Formula
   desc "a file watch tool for rapid bazel development"
   homepage "https://github.com/meetup/rebazel"
-  url "https://github.com/meetup/rebazel/releases/download/v0.1.1/rebazel-Darwin-x86_64.tar.gz"
-  sha256 "6668f6a87a2e50e5dff3c93d2e8bf11c699aaf26252742ec6c66ea6306d3b59e"
-  version "v0.1.1"
+  url "https://github.com/meetup/rebazel/releases/download/v0.1.3/rebazel-Darwin-x86_64.tar.gz"
+  sha256 "9bb1d8c234c0bfea0d25732547a708dd6ef7dd895f7aa41c85476a226ddf47d1"
+  version "v0.1.3"
 
   bottle :unneeded
 
